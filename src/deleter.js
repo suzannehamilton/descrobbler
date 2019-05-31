478aa0265e9c63842abe485894a1d2e8b987baff
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2019-05-31 22:03:30 +0100

    Update delete button selectors

--- src/deleter.js
+++ src/deleter.js
@@ -2,7 +2,7 @@
   var duplicates = unscrobbler.findDuplicates();
 
   duplicates.forEach(function (scrobble) {
-    var deleteLink = scrobble.querySelector('.chartlist-more-menu form[action$=unscrobble] button');
+    var deleteLink = scrobble.querySelector('.chartlist-more form[action$=delete] button');
     deleteLink.click();
   });
 }());
